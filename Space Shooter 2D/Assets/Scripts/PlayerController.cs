0bba7db3c872baee6cdfeeddb2b037b32ad87863
ika25 <lomiika1990@yahoo.com>
2020-08-07 11:47:24 +0100

    Added Background

--- Space Shooter 2D/Assets/Scripts/PlayerController.cs
+++ Space Shooter 2D/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;//Get inputs form both axis + PLayer speed
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.position.x, topRightLimit.position.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.position.y, topRightLimit.position.y), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.position.x, topRightLimit.position.x), 
+            Mathf.Clamp(transform.position.y, bottomLeftLimit.position.y, topRightLimit.position.y), transform.position.z);//clamps player movement so it wont desapper from the frame(keeping player on screen)
     }
 }
