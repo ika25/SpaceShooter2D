32a809fd4b18fb1bdde60f7992b7da7ae5011848
ika25 <lomiika1990@yahoo.com>
2020-08-12 13:41:45 +0100

    Player health system added

---
+++ Space Shooter 2D/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager instance;
+
+    public int currentHealth;
+    public int maxHealth;
+
+    public GameObject deathEffect;
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+
+            instance = this;
+
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    
+    public void HurtPlayer()
+    {
+        currentHealth-=1;
+
+        if(currentHealth <= 0)
+        {
+            Instantiate(deathEffect, transform.position, transform.rotation);
+
+            gameObject.SetActive(false);//deactivates from world which means we cant d anything for sshort amount of time.
+        }
+    }
+}
