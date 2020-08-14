2fffeb037df1a525809eaffff548907ebcac0b35
ika25 <lomiika1990@yahoo.com>
2020-08-14 18:19:49 +0100

    added Enemy movement

--- Space Shooter 2D/Assets/Scripts/EnemyController.cs
+++ Space Shooter 2D/Assets/Scripts/EnemyController.cs
@@ -6,6 +6,12 @@ public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
 
+    public Vector2 startDirection;
+
+    public bool shouldChangeDirection;
+    public float changeDirectionXPoint;
+    public Vector2 changeDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +21,22 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        //transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        if(!shouldChangeDirection)
+        {
+            transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            if(transform.position.x > changeDirectionXPoint)
+            {
+                transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0f);
+            }
+            else
+            {
+                transform.position += new Vector3(changeDirection.x * moveSpeed * Time.deltaTime, changeDirection.y * moveSpeed * Time.deltaTime, 0f);
+            }
+        }
     }
 
     private void OnBecameInvisible()//soon as object becomes invisible destroy object
