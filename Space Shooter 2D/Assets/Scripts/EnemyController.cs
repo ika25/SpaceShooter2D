69c4139a2a2237d8042b1f9849ed4dcba4144bce
ika25 <lomiika1990@yahoo.com>
2020-08-14 19:13:21 +0100

    enemies can shoot back now

--- Space Shooter 2D/Assets/Scripts/EnemyController.cs
+++ Space Shooter 2D/Assets/Scripts/EnemyController.cs
@@ -12,10 +12,18 @@ public class EnemyController : MonoBehaviour
     public float changeDirectionXPoint;
     public Vector2 changeDirection;
 
+    public GameObject shotToFire;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public bool canShoot;
+    private bool allowShooting;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = timeBetweenShots;  
     }
 
     // Update is called once per frame
@@ -37,10 +45,28 @@ public class EnemyController : MonoBehaviour
                 transform.position += new Vector3(changeDirection.x * moveSpeed * Time.deltaTime, changeDirection.y * moveSpeed * Time.deltaTime, 0f);
             }
         }
+
+        if (allowShooting)
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                Instantiate(shotToFire, firePoint.position, firePoint.rotation);
+            }
+        }
     }
 
     private void OnBecameInvisible()//soon as object becomes invisible destroy object
     {
         Destroy(gameObject);
+    }
+
+    private void OnBecameVisible()
+    {
+        if(canShoot)
+        {
+            allowShooting = true;
+        }
     }
 }
