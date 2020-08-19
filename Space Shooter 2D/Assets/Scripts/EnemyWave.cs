6d285bdf3ed06d98bdc61f64150d379b7dcdd39b
ika25 <lomiika1990@yahoo.com>
2020-08-19 13:05:59 +0100

    Added Enemy Wave Script

---
+++ Space Shooter 2D/Assets/Scripts/EnemyWave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DetachChildren();//This makes it so that all the enemies that are part of the wave are no longer a child of the object.
+        Destroy(gameObject);//we know they dont have children anymore so its safe to remove that object from the world 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
