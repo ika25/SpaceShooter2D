191e55fe7a2dbbddf6394221cedd07048326aa36
ika25 <lomiika1990@yahoo.com>
2020-08-23 12:35:20 +0100

    Setting up Game Over Screen

--- Space Shooter 2D/Assets/Scripts/GameManager.cs
+++ Space Shooter 2D/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     {
         currentLives--;
 
-        if(currentLives > 0)
+        if (currentLives > 0)
         {
             //respawn code
             StartCoroutine(RespawnCo());
