971a696ca44f65d4008be3a02b1909f052ae4d96
ika25 <lomiika1990@yahoo.com>
2020-08-28 16:30:28 +0100

    Volume Slider added

---
+++ Space Shooter 2D/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    public Slider Volume;
+    public AudioSource myMusic;
+
+    void Update()
+    {
+        myMusic.volume = Volume.value;
+    }
+
+
+    //public AudioMixer audioMixer;
+
+    //public void SetVolume (float volume)
+    //{
+    //audioMixer.SetFloat("volume", volume);
+    //}
+}
